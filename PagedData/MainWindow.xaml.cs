4ca3feb8dcc065d034f0164b0274c0d84b1263fb
kelvince pericles <kelvince05@outlook.com>
2021-09-22 10:32:54 +0300

    Second commit

---
+++ PagedData/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace PagedData
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+  }
+}
