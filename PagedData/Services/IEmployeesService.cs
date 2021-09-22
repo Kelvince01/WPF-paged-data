c632a9a358c8ba39ef7693256721895b86f6ff7d
kelvince pericles <kelvince05@outlook.com>
2021-09-22 10:31:30 +0300

    Initial commit

---
+++ PagedData/Services/IEmployeesService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using DynamicData;
+using PagedData.Models;
+
+namespace PagedData.Services
+{
+    public interface IEmployeesService
+    {
+        IObservable<IChangeSet<Employee, int>> EmployeesConnection();
+        void LoadData();
+    }
+}
---
+++ PagedData/Utilities/ViewModelLocator.cs
@@ -0,0 +1,19 @@
+﻿using PagedData.Services;
+using PagedData.ViewModels;
+using Unity;
+
+namespace PagedData.Utilities
+{
+    public class ViewModelLocator
+    {
+        private readonly UnityContainer container;
+
+        public ViewModelLocator()
+        {
+            container = new UnityContainer();
+            container.RegisterType<IEmployeesService, EmployeesService>();
+        }
+
+        public MainWindowViewModel MainWindowVM => container.Resolve<MainWindowViewModel>();
+    }
+}
